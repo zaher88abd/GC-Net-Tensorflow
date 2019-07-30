f4f6f980279d7a34b4cb9247b0a1798ac1f20e5d
Zaher <zaher88abd@gmail.com>
2019-07-30 12:10:27 -0300

    add job.sh file and python req file

---
+++ tensorflow_job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1        # request GPU "generic resource"
+#SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=32000M        # memory per node
+#SBATCH --time=01:00      # time (DD-HH:MM)
+#SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
+
+
+source ../pytf/bin/activate
+tensorboard --logdir=/log/ --host 0.0.0.0 &
+python train.py
