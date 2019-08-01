abc22f8e075dc65b696ca998fd67c390798d4a8f
Zaher <zaher88abd@gmail.com>
2019-08-01 12:17:55 -0300

    update the tfreader to tf.data.tfreadData

--- util.py
+++ util.py
@@ -4,7 +4,35 @@ Created on Tue May 15 22:24:05 2018
 
 @author: Kel
 """
+from params import Params
 import tensorflow as tf
+
+
+def _parse_function(proto):
+    params = Params()
+    width, height = params.original_w, params.original_h
+    batch_size = params.batch_size
+    target_w, target_h = params.target_w, params.target_h
+
+    # define your tfrecord again. Remember that you saved your image as a string.
+    features = {
+        'img_left': tf.FixedLenFeature([], tf.string),
+        'img_right': tf.FixedLenFeature([], tf.string),
+        'disparity': tf.FixedLenFeature([], tf.string)
+    }
+
+    # Load one example
+    parsed_features = tf.parse_single_example(proto, features)
+
+    # Turn your saved image string into an array
+    # parsed_features['image'] = tf.decode_raw(
+    #     parsed_features['image'], tf.uint8)
+
+    image_left = tf.decode_raw(parsed_features['img_left'], tf.uint8)
+    image_right = tf.decode_raw(parsed_features['img_right'], tf.uint8)
+    disparity = tf.decode_raw(parsed_features['disparity'], tf.float32)
+
+    return image_left, image_right, disparity
 
 
 def read_and_decode(params, filename):
@@ -12,40 +40,37 @@ def read_and_decode(params, filename):
     batch_size = params.batch_size
     target_w, target_h = params.target_w, params.target_h
 
-    filename_queue = tf.train.string_input_producer([filename])
+    # filename_queue = tf.train.string_input_producer([filename])
 
-    reader = tf.TFRecordReader()
+    dataset = tf.data.TFRecordDataset([filename])
 
-    _, serialized_example = reader.read(filename_queue)
 
-    features = tf.parse_single_example(
-        serialized_example,
+    # Maps the parser on every filepath in the array. You can set the number of parallel loaders here
+    dataset = dataset.map(_parse_function, num_parallel_calls=2)
 
-        features={
-            'img_left': tf.FixedLenFeature([], tf.string),
-            'img_right': tf.FixedLenFeature([], tf.string),
-            'disparity': tf.FixedLenFeature([], tf.string)
-        })
+    dataset = dataset.repeat()
 
-    image_left = tf.decode_raw(features['img_left'], tf.uint8)
-    image_left = tf.reshape(image_left, [height, width, 3])
+    dataset = dataset.shuffle(1)
 
-    image_right = tf.decode_raw(features['img_right'], tf.uint8)
-    image_right = tf.reshape(image_right, [height, width, 3])
+    # Set the batchsize
+    dataset = dataset.batch(batch_size)
 
-    disparity = tf.decode_raw(features['disparity'], tf.float32)
-    disparity = tf.reshape(disparity, [height, width, 1])
+    # Create an iterator
+    iterator = dataset.make_one_shot_iterator()
+
+    # Create your tf representation of the iterator
+    image_left_batch, image_right_batch, disparity_batch = iterator.get_next()
+
+    image_left = tf.reshape(image_left_batch, [batch_size, height, width, 3])
+
+    image_right = tf.reshape(image_right_batch, [batch_size, height, width, 3])
+
+    disparity = tf.reshape(disparity_batch, [batch_size, height, width, 1])
 
     # Convert from [0, 255] -> [-0.5, 0.5] floats.
     image_left = tf.cast(image_left, tf.float32) * (1. / 255) - 0.5
     image_right = tf.cast(image_right, tf.float32) * (1. / 255) - 0.5
+    concat = tf.concat([image_left, image_right, disparity], 3)
+    img_crop = tf.random_crop(concat, [batch_size,target_h, target_w, 7])
 
-    concat = tf.concat([image_left, image_right, disparity], 2)
-    img_crop = tf.random_crop(concat, [target_h, target_w, 7])
-
-    image_left_batch, image_right_batch, disparity_batch = tf.train.shuffle_batch(
-        [img_crop[:, :, 0:3], img_crop[:, :, 3:6], img_crop[:, :, 6:]],
-        batch_size=batch_size, capacity=50,
-        min_after_dequeue=10, num_threads=2)
-
-    return [image_left_batch, image_right_batch, disparity_batch]
+    return img_crop[:, :, :, 0:3], img_crop[:, :, :, 3:6], img_crop[:, :, :, 6:]
