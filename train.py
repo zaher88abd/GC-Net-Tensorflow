09620097a63190edea0feecee1285839d1459772
Zaher <zaher88abd@gmail.com>
2019-08-08 18:21:47 -0300

    add fit time

--- train.py
+++ train.py
@@ -18,14 +18,14 @@ train_dir = 'saved_model/'
 train_zed_dir = 'saved_zed_model/'
 train_backup_dir = 'backup_saved_model/'
 
-data_record = ["dataset/my_nn_train.tfrecords", "dataset/my_nn_test.tfrecords"]
+data_record = ["dataset/fly_train.tfrecords", "dataset/fly_test.tfrecords"]
 
 p = params.Params()
 
 # Check this befor start training
 p.start_from_backup_model = False
-batch_train = util.read_and_decode(p, data_record[0], my_data=True)
-batch_test = util.read_and_decode(p, data_record[1], my_data=True)
+batch_train = util.read_and_decode(p, data_record[0], my_data=False)
+batch_test = util.read_and_decode(p, data_record[1], my_data=False)
 
 img_L = tf.placeholder(tf.float32, [p.batch_size, p.target_h, p.target_w, 3])
 img_R = tf.placeholder(tf.float32, [p.batch_size, p.target_h, p.target_w, 3])
@@ -60,6 +60,8 @@ saver = tf.train.Saver()
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 print("**** the learning rate is ", learning_rate, " ****")
+import time
+
 with tf.Session() as sess:
     summary_writer_train = tf.summary.FileWriter("./log/nn_zed/train", graph=tf.get_default_graph())
     summary_writer_test = tf.summary.FileWriter("./log/nn_zed/test", graph=tf.get_default_graph())
@@ -77,19 +79,25 @@ with tf.Session() as sess:
         sess.run(init)
 
     coord = tf.train.Coordinator()
+    duration = 0
+    duration_avg = 0
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
     for step in range(180001):
+        start = time.time()
         batch = sess.run(batch_train)
         feed_dict = {img_L: batch[0], img_R: batch[1], disp: batch[2], phase: True}
-
         #        _, loss_value, sample_dis, sample_gt = sess.run([train_op, loss,
         #        pred[0, 100, 100, :], disp[0, 100, 100,:]], feed_dict=feed_dict)
         _, loss_value, glb_step, loss_s = sess.run([train_op, loss, global_step, loss_summary], feed_dict=feed_dict)
+        end = time.time()
+        duration += (end - start)
+        duration_avg += (end - start)
         summary_writer_train.add_summary(loss_s, glb_step)
         if glb_step % 2 == 0 and step > 0:
             #            print('Step %d: training loss = %.2f | sample disparity: %.2f |
             #            ground truth: %.2f' % (step, loss_value, sample_dis, sample_gt))
-            print('Step %d: training loss = %.2f' % (glb_step, loss_value))
+            print('Step %d: training loss = %.2f time= %.3f' % (glb_step, loss_value, duration/2))
+            duration = 0
             # train_loss_.append([loss_value, glb_step])
 
         if glb_step % 50 == 0 and step > 0:
@@ -105,7 +113,9 @@ with tf.Session() as sess:
                 summary_writer_test.add_summary(gs_s, glb_step + j)
                 summary_writer_test.add_summary(gt_s, glb_step + j)
             test_total_loss = test_total_loss / 10
-            print('------------------  Step %d: test loss = %.2f ------------------' % (glb_step, test_total_loss))
+            print('------------------  Step %d: test loss = %.2f time = %0.3f------------------' % (
+                glb_step, test_total_loss, duration_avg / 50))
+            duration_avg = 0
             saver.save(sess, train_backup_dir, global_step=global_step)
 
             if glb_step % (500 * 5) == 0 and step > 0:
